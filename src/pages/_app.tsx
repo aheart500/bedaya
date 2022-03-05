09093c05efa5ba10afe5d564081a0d4f6fd1409b
Nasser <aheart500@yahoo.com>
2022-03-06 00:13:25 +0200

    Added Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider, CssBaseline } from "@mui/material";
 import createEmotionCache from "../utility/createEmotionCache";
 import lightTheme from "../styles/theme/lightTheme";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -14,6 +15,10 @@ const MyApp = (props) => {
   return (
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={lightTheme}>
+        <Head>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <title>Bedaya</title>
+        </Head>
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { Button, TextField } from "@mui/material";
 
 const IndexPage = () => {
-  return <TextField variant="outlined" label="Nasser" />;
+  return (
+    <div>
+      <TextField variant="outlined" label="Nasser" />;
+      <img src="/images/logo.png" />
+    </div>
+  );
 };
 
 export default IndexPage;
